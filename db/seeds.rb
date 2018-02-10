6e698ce711815c9136cc46a110637b01fcf46efc
Samantha Smith <samantha.coderedtech@gmail.com>
2018-02-10 12:46:45 -0500

    added_new_products_pizza

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!")
+Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!" image: "https://unsplash.com/photos/O4XEF-rkoQU")
