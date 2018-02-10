35b0aae843884beec1b4ed73fa09c498ff50b742
Samantha Smith <samantha.coderedtech@gmail.com>
2018-02-10 14:13:55 -0500

    experimenting with adding products valentine flowers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
-Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!" image: "https://unsplash.com/photos/O4XEF-rkoQU")
+Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!", image_url: "http://media.istockphoto.com/photos/kitten-on-a-white-background-picture-id511458158?k=6&m=511458158&s=170667a&w=0&h=YS27-I0nj5x89iDRUZ2YxE77hEghPb0HNCWwPCrMmn8=")
+
+Product.create(name: "made up seed product", description: "Valentine's bouquet", image_url: "https://www.stockphoto.com/photo/OTY1NzE5NzUwMDAxMWY1YmNmYjBlZA==/Businessman%20attracting%20money%20with%20magnet/")
