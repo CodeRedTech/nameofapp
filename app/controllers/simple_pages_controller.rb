b09c234dfd9b49b427a2165a97796119dd098379
Samantha Smith <samantha.coderedtech@gmail.com>
2018-02-18 18:19:49 -0500

    updated products and product images

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -4,6 +4,7 @@ class SimplePagesController < ApplicationController
     
   def landing_page
     @products = Product.limit(3)
+    @featured_product = Product.last
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
-Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!", image_url: "http://media.istockphoto.com/photos/kitten-on-a-white-background-picture-id511458158?k=6&m=511458158&s=170667a&w=0&h=YS27-I0nj5x89iDRUZ2YxE77hEghPb0HNCWwPCrMmn8=")
+Product.create(name: "Coding", description: "10 hours of web development!", image_url:"codingpx.jpg")
 
-Product.create(name: "made up seed product", description: "Valentine's bouquet", image_url: "https://www.stockphoto.com/photo/OTY1NzE5NzUwMDAxMWY1YmNmYjBlZA==/Businessman%20attracting%20money%20with%20magnet/")
+Product.create(name: "Diamond 1", description: "Rose gold diamond ring", image_url:"https://images.unsplash.com/photo-1514846376753-cd076d84e2f7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2ecbda9d6b5680393603857ddeadc747&auto=format&fit=crop&w=750&q=80")
+
+Product.create(name: "Diamond 2", description: "White diamond ring", image_url:"https://images.unsplash.com/photo-1470321386428-4b1668f21ad3?ixlib=rb-0.3.5&s=b5e2892fd762c1b7cd53304b72e2a230&auto=format&fit=crop&w=750&q=80")
