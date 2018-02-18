b54de56be9da8078357d9713b4f6726dfad4015f
Samantha Smith <samantha.coderedtech@gmail.com>
2018-02-17 23:46:31 -0500

    price migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180204171830) do
+ActiveRecord::Schema.define(version: 20180218043307) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180204171830) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "color"
+    t.string "price"
   end
 
 end
