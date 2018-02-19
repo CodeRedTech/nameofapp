2114465e55794b39079eb18b72934ae4488f2283
Samantha Smith <samantha.coderedtech@gmail.com>
2018-02-18 22:46:13 -0500

    added orders to seeds and added a list of orders to orders/index.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,7 @@ Product.create(name: "Coding", description: "10 hours of web development!", imag
 Product.create(name: "Diamond 1", description: "Rose gold diamond ring", image_url:"https://images.unsplash.com/photo-1514846376753-cd076d84e2f7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2ecbda9d6b5680393603857ddeadc747&auto=format&fit=crop&w=750&q=80")
 
 Product.create(name: "Diamond 2", description: "White diamond ring", image_url:"https://images.unsplash.com/photo-1470321386428-4b1668f21ad3?ixlib=rb-0.3.5&s=b5e2892fd762c1b7cd53304b72e2a230&auto=format&fit=crop&w=750&q=80")
+
+Order.create(user_id: 1, product_id: 1, total: 10.0)
+
+Order.create(user_id: 1, product_id: 2, total: 5.0)
