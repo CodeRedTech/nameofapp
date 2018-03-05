c479c2f1f3a5a0d9ebad91080b29e93b575a02cc
Samantha Smith <samantha.coderedtech@gmail.com>
2018-03-05 12:27:24 -0500

    5.9 redo validation pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -29,16 +29,17 @@ class ProductsController < ApplicationController
 
   # POST /products
   # POST /products.json
-  def create
-    @product = Product.new(product_params)
-
+def create
+    @product = Product.find(params[:product_id])
+    @comment = @product.comments.new(comment_params)
+    @comment.user = current_user
     respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+      if @comment.save
+        format.html { redirect_to @product, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
       else
-        format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
+        format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
+   validates :name, presence: true
+     validates :user, presence: true
+     validates :product, presence: true
+     validates :rating, numericality: { only_integer: true }
+There’s one thing to keep in mind here: as we said earlier, we may end up usi
+end
    has_many :orders
    has_many :comments
    def self.search(search_term)
