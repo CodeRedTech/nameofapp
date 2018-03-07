170367a00fa898722267c348d1eeadfbd197dcce
Samantha Smith <samantha.coderedtech@gmail.com>
2018-03-07 17:26:56 -0500

    syntax error in product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,17 @@
 class Product < ApplicationRecord
-   validates :name, presence: true
-     validates :user, presence: true
-     validates :product, presence: true
-     validates :rating, numericality: { only_integer: true }
-There’s one thing to keep in mind here: as we said earlier, we may end up usi
-end
-   has_many :orders
-   has_many :comments
-   def self.search(search_term)
-   Product.where("name LIKE ?", "%#{search_term}%")
+  has_many :orders
+  has_many :comments
+  validates :name, presence: true
+
+  def self.search(search_term)
+    unless Rails.env.production?
+          Product.where("name LIKE ?", "%#{search_term}%")
+        else
+          Product.where("name ilike ?", "%#{search_term}%")
+        end
   end
     
-   def highest_rating_comment
+  def highest_rating_comment
    comments.rating_desc.first
   end
     
@@ -20,9 +20,8 @@ end
   end
 
   def average_rating
-  comments.average(:rating).to_f
- end
-    
+    comments.average(:rating).to_f
+  end
 end
 
 
