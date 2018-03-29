9c6d144379025e6463576eb7ebcb0d74b87176ef
Samantha Smith <samantha.coderedtech@gmail.com>
2018-03-29 08:05:45 -0400

    byebug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+ 
+  def new
+    byebug
+    @person = Person.new
+  end
     
   rescue_from CanCan::AccessDenied do |exception|
   redirect_to main_app.root_url, alert: exception.message
