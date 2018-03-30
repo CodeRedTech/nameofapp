c0b69cdd1e606ebae795a4b6fcc2346fbe8f17e6
Samantha Smith <samantha.coderedtech@gmail.com>
2018-03-30 12:01:08 -0400

    updates to product spec and users controller

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,24 +1,19 @@
 require 'rails_helper'
 
 describe Product do
-  let(:product) { Product.create!(name: "race bike") }
-
-let(:user) {User.create! (email:"samantha@coderedtech.net", password:"Charlos2")}
-
   before do
-    product.comments.create!(rating: 1, user: user, body: "Awful bike!")
-    product.comments.create!(rating: 3, user: user, body: "Ok bike!")
-    product.comments.create!(rating: 5, user: user, body: "Great bike!")
+    @product = Product.create!(name: "race bike")
+    @user = User.create!(email:"samantha@coderedtech.net", password:"Charlos2")
+    @product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
+    @product.comments.create!(rating: 3, user: @user, body: "Ok bike!")
+    @product.comments.create!(rating: 5, user: @user, body: "Great bike!")
   end
 
   it "returns the average rating of all comments" do
-    expect(Product.new).to eq 3
+    expect(@product.average_rating).to eq 3
   end
 
   it "is not valid without a name" do
-    expect(Product.new(description: "Nice bike")).not_to be_valid
+    expect(Product.new(description: "Nice bike")).to_not be_valid
   end
-
-   
-  
 end
