df784ec700621c47f62a423a253cc81887913686
Samantha Smith <samantha.coderedtech@gmail.com>
2018-04-20 12:26:56 -0400

    fixing syntax error in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,5 +103,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.web_socket_server_url = "wss://coderedtechapps.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://coderedtechapps.herokuapp.com', 'http://coderedtechapps.herokuapp.com'
+  config.action_cable.allowed_request_origins = ['https://coderedtechapps.herokuapp.com', 'http://coderedtechapps.herokuapp.com']
 end
