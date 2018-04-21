8b08ac804ee4992b08a70acd7ef428bfa3744e79
Samantha Smith <samantha.coderedtech@gmail.com>
2018-04-21 15:15:21 -0400

    chosen plugin

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -2,8 +2,6 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
   connected: function() {
     // Called when the subscription is ready for use on the server
   },
-    
-ActionCable.server.broadcast 'product_channel', comment: "Hello World", average_rating: 5
 
   disconnected: function() {
     // Called when the subscription has been terminated by the server
--- app/assets/javascripts/helloworld.js
+++ app/assets/javascripts/helloworld.js
@@ -1,6 +1,4 @@
-$(document).ready(function() {
-  console.log("Hello world!");
-});
+console.log("Hello world");
 
 
 
