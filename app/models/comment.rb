83b7780864617837ad460a6b12ec15b15a54256a
Samantha Smith <samantha.coderedtech@gmail.com>
2018-05-07 19:30:33 -0400

    Advanced ActionCable and ActiveJob

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,5 @@ class Comment < ApplicationRecord
   belongs_to :product
   scope :rating_desc, -> { order(rating: :desc) }
   scope :rating_asc, -> { order(rating: :asc) }
+  after_create_commit { CommentUpdateJob.perform_later(self, self.user) }
 end
