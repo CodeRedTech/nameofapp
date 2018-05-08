0808041d673390d57ae9ef52c26ef87c30ce8740
Samantha Smith <samantha.coderedtech@gmail.com>
2018-05-08 13:28:24 -0400

    updates to products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-Product.create(name: "Coding", description: "10 hours of web development!", image_url:"codingpx.jpg", price: "100")
+Product.create(name: "Diamond 1", description: "white gold band and white diamond", image_url:"codingpx.jpg", price: "5000")
 
-Product.create(name: "Diamond 1", description: "Rose gold diamond ring", image_url:"https://images.unsplash.com/photo-1514846376753-cd076d84e2f7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2ecbda9d6b5680393603857ddeadc747&auto=format&fit=crop&w=750&q=80", price: "100")
+Product.create(name: "Diamond 2", description: "Rose gold diamond ring", image_url:"https://images.unsplash.com/photo-1514846376753-cd076d84e2f7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2ecbda9d6b5680393603857ddeadc747&auto=format&fit=crop&w=750&q=80", price: "3400")
 
-Product.create(name: "Diamond 2", description: "White diamond ring", image_url:"https://images.unsplash.com/photo-1470321386428-4b1668f21ad3?ixlib=rb-0.3.5&s=b5e2892fd762c1b7cd53304b72e2a230&auto=format&fit=crop&w=750&q=80", price: "100")
+Product.create(name: "Diamond 3", description: "White diamond ring", image_url:"https://images.unsplash.com/photo-1470321386428-4b1668f21ad3?ixlib=rb-0.3.5&s=b5e2892fd762c1b7cd53304b72e2a230&auto=format&fit=crop&w=750&q=80", price: "2700")
 
 Order.create(user_id: 1, product_id: 1, total: 10.0)
 
