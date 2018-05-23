3c2121042a1a9aa68e8f558e55bbfa858ef7e53e
Samantha Smith <samantha.coderedtech@gmail.com>
2018-05-23 13:51:07 -0400

    first draft unit tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe Comment, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
+describe Comment do
+  context 'comment created' do
+    let(:product) { FactoryBot.create(:product) }
+    let(:user) { FactoryBot.create(:user) }
+
+    it 'contains body, rating, user, and product' do
+      expect(Comment.new(body: 'body body body', rating: 3, user: user, product: product)).to be_valid
+      expect(Comment.new(body: 'body body body', rating: 3, user: user)).not_to be_valid
+      expect(Comment.new(body: 'body body body', rating: 3, product: product)).not_to be_valid
+      expect(Comment.new(body: 'body body body', user: user, product: product)).not_to be_valid
+      expect(Comment.new(rating: 3, user: user, product: product)).not_to be_valid
+    end
+
+    it 'is not valid unless rating is a number' do
+      expect(Comment.new(body: 'body body body', rating: 3, user: user, product: product)).to be_valid
+      expect(Comment.new(body: 'body body body', rating: true, user: user, product: product)).not_to be_valid
+      expect(Comment.new(body: 'body body body', rating: { rating: 3 }, user: user, product: product)).not_to be_valid
+      expect(Comment.new(body: 'body body body', rating: 'three', user: user, product: product)).not_to be_valid
+    end
+
+    it 'is only valid if rating is within 0 and 5' do
+      expect(Comment.new(body: 'body body body', rating: 3, user: user, product: product)).to be_valid
+      expect(Comment.new(body: 'body body body', rating: 1, user: user, product: product)).to be_valid
+      expect(Comment.new(body: 'body body body', rating: 5, user: user, product: product)).to be_valid
+      expect(Comment.new(body: 'body body body', rating: -4, user: user, product: product)).not_to be_valid
+      expect(Comment.new(body: 'body body body', rating: 0, user: user, product: product)).not_to be_valid
+      expect(Comment.new(body: 'body body body', rating: 6, user: user, product: product)).not_to be_valid
+    end
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,19 +1,48 @@
 require 'rails_helper'
 
 describe Product do
-  before do
-    @product = Product.create!(name: "race bike", description: "This is a test", price: 200)
-    @user = User.create!(email:"samantha@coderedtech.net", password:"Charlos2")
-    @product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
-    @product.comments.create!(rating: 3, user: @user, body: "Ok bike!")
-    @product.comments.create!(rating: 5, user: @user, body: "Great bike!")
+  context 'when the product has comments' do
+    let(:product) { FactoryBot.create(:product) }
+    let(:user) { FactoryBot.create(:user) }
+
+    before do
+      product.comments.create!(rating: 1, user: user, body: 'Awful bike!')
+    end
+    before do
+      product.comments.create!(rating: 3, user: user, body: 'Ok bike!')
+    end
+    before do
+      product.comments.create!(rating: 5, user: user, body: 'Great bike!')
+    end
+
+    it 'returns the average rating of all comments' do
+      expect(product.average_rating).to eq 3
+    end
+
+    it 'returns lowest rating' do
+      expect(product.lowest_rating_comment.body).to eq 'Awful bike!'
+    end
+
+    it 'returns highest rating' do
+      expect(product.highest_rating_comment.body).to eq 'Great bike!'
+    end
   end
 
-  it "returns the average rating of all comments" do
-    expect(@product.average_rating).to eq 3
-  end
+  context 'product created' do
+    it 'is valid only if image_url, name, and price_in_pennies are present' do
+      expect(Product.new(image_url: 'houjicha.jpg', name: 'jaja', price_in_pennies: '3')).to be_valid # OK
+      expect(Product.new(image_url: 'houjicha.jpg', name: 'jaja')).not_to be_valid # missing price_in_pennies
+      expect(Product.new(image_url: 'houjicha.jpg', price_in_pennies: '3')).not_to be_valid # missing name
+      expect(Product.new(name: 'jaja', price_in_pennies: '3')).not_to be_valid # missing image_url
+    end
 
-  it "is not valid without a name" do
-    expect(Product.new(description: "Nice bike")).to_not be_valid
+    it 'is not valid unless price_in_pennies is a positive number' do
+      expect(Product.new(name: 'tea', image_url: 'houjicha.jpg', price_in_pennies: '8')).to be_valid # OK
+      expect(Product.new(name: 'tea', image_url: 'houjicha.jpg', price_in_pennies: 'cat')).not_to be_valid # string
+      expect(Product.new(name: 'tea', image_url: 'houjicha.jpg', price_in_pennies: true)).not_to be_valid # boolean
+      expect(Product.new(name: 'tea', image_url: 'houjicha.jpg', price_in_pennies: '')).not_to be_valid # empty string
+      expect(Product.new(name: 'tea', image_url: 'houjicha.jpg', price_in_pennies: '8.33')).not_to be_valid # not integer
+      expect(Product.new(name: 'tea', image_url: 'houjicha.jpg', price_in_pennies: -4)).not_to be_valid # negative integer
+    end
   end
 end
